593a67d0aa3fd3586272bd6573698afb0289070e
weihuizhang@huashenghaoche.com <weihuizhang@huashenghaoche.com>
2019-06-17 11:33:20 +0800

    修改了DateTimeFormatterTest

--- src/test/java/com/zwh/javasamples/DateTimeFormatterTest.java
+++ src/test/java/com/zwh/javasamples/DateTimeFormatterTest.java
@@ -2,6 +2,7 @@ package com.zwh.javasamples;
 
 import java.text.ParseException;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -16,37 +17,67 @@ import java.util.concurrent.TimeUnit;
  */
 public class DateTimeFormatterTest {
 
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter YMDHMS = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter YMD = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     /**
-     * 从 Date --> Instant --> LocalDateTime --> String
+     * 格式化带时分秒的日期
+     * Date --> Instant --> LocalDateTime --> String
+     *
+     * @param date
+     * @return
+     */
+    public static String formatDateTime(Date date) {
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        return YMDHMS.format(localDateTime);
+    }
+
+    /**
+     * 格式化不带时分秒的日期
+     * Date --> Instant --> ZonedDateTime --> LocalDate --> String
      *
      * @param date
      * @return
      */
     public static String formatDate(Date date) {
-        LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
-        return formatter.format(localDateTime);
-    }
-
-    public static String formatDate2(LocalDateTime date) {
-        return formatter.format(date);
-    }
-
-    public static LocalDateTime parse2(String dateNow) {
-        return LocalDateTime.parse(dateNow, formatter);
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return YMD.format(localDate);
     }
 
     /**
-     * 从 String--> LocalDateTime --> Instant --> Date
+     * 转换带时分秒的日期字符串
+     * String--> LocalDateTime --> Instant --> Date
      *
      * @param dateStr
+     * @param formatter YMDHMS
      * @return
      */
-    public static Date parse(String dateStr) {
+    public static Date parseDateTime(String dateStr, DateTimeFormatter formatter) {
         LocalDateTime localDateTime = LocalDateTime.parse(dateStr, formatter);
         Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
         return Date.from(instant);
+    }
+
+    /**
+     * 转换不带时分秒的字符串
+     * String --> LocalDate --> Instant --> Date
+     *
+     * @param dateStr
+     * @param formatter YMD
+     * @return
+     */
+    public static Date parseDate(String dateStr, DateTimeFormatter formatter){
+        LocalDate localDate = LocalDate.parse(dateStr, formatter);
+        Instant instant = localDate.atStartOfDay(ZoneId.systemDefault()).toInstant();
+        return Date.from(instant);
+    }
+
+    public static String formatDate2(LocalDateTime date) {
+        return YMDHMS.format(date);
+    }
+
+    public static LocalDateTime parse2(String dateNow) {
+        return LocalDateTime.parse(dateNow, YMDHMS);
     }
 
     public static void main(String[] args) throws InterruptedException, ParseException {
