9841df86e0544ee500d536b265f7f01061d9c520
weihuizhang@huashenghaoche.com <weihuizhang@huashenghaoche.com>
2019-07-03 09:52:07 +0800

    添加了NetworkServiceBo接口

---
+++ src/main/java/com/zwh/sdk/NetworkServiceBO.java
@@ -0,0 +1,14 @@
+package com.zwh.sdk;
+
+/**
+ * @author ZhangWeihui
+ * @date 2019/7/2 14:48
+ */
+public interface NetworkServiceBO {
+
+    String connect();
+
+    String disconnect();
+
+    String reconnect();
+}
