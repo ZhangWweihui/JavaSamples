8d579654e4521149c1b5353121183c656f27a195
weihuizhang@huashenghaoche.com <weihuizhang@huashenghaoche.com>
2019-07-08 15:38:50 +0800

    leetcode两数相加提交通过

--- src/main/java/com/zwh/leetcode/TwoNumSumAction.java
+++ src/main/java/com/zwh/leetcode/TwoNumSumAction.java
@@ -1,5 +1,7 @@
 package com.zwh.leetcode;
 
+import org.springframework.util.StringUtils;
+
 import java.math.BigDecimal;
 
 /**
@@ -20,21 +22,70 @@ import java.math.BigDecimal;
  */
 public class TwoNumSumAction {
 
+    /**
+     * 解法一
+     * @param l1
+     * @param l2
+     * @return
+     */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         BigDecimal sum = BigDecimal.ZERO;
+        BigDecimal ten = BigDecimal.TEN;
         int index = 0;
         do {
-            sum = sum.add(BigDecimal.valueOf(l1.val).multiply(BigDecimal.valueOf(Math.pow(10,index++))));
+            sum = sum.add(BigDecimal.valueOf(l1.val).multiply(ten.pow(index++)));
             l1 = l1.next;
         }while(l1!=null);
 
         index = 0;
         do {
-            sum = sum.add(BigDecimal.valueOf(l2.val).multiply(BigDecimal.valueOf(Math.pow(10,index++))));
+            sum = sum.add(BigDecimal.valueOf(l2.val).multiply(ten.pow(index++)));
             l2 = l2.next;
         }while(l2!=null);
 
+        System.out.println(sum.toPlainString());
         char[] chars = sum.setScale(0).toPlainString().toCharArray();
+        ListNode firstNode = null;
+        ListNode preNode = null;
+        for(int i=chars.length-1;i>=0;i--){
+            ListNode ln = new ListNode(chars[i]-48);
+            if(i==chars.length-1){
+                firstNode = ln;
+                preNode = ln;
+            } else {
+                preNode.next = ln;
+                preNode = ln;
+            }
+        }
+        return firstNode;
+    }
+
+    /**
+     * 解法二，比解法一效率高
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        StringBuffer buf = new StringBuffer();
+        do {
+            buf.append(l1.val);
+            l1 = l1.next;
+        }while(l1!=null);
+        buf.reverse();
+        System.out.println(buf);
+        BigDecimal n1 = new BigDecimal(buf.toString());
+
+        buf.delete(0,buf.length());
+        do {
+            buf.append(l2.val);
+            l2 = l2.next;
+        }while(l2!=null);
+        buf.reverse();
+        System.out.println(buf);
+        BigDecimal n2 = new BigDecimal(buf.toString());
+
+        char[] chars = n1.add(n2).setScale(0).toPlainString().toCharArray();
         ListNode firstNode = null;
         ListNode preNode = null;
         for(int i=chars.length-1;i>=0;i--){
@@ -56,12 +107,12 @@ public class TwoNumSumAction {
         ListNode(int x) { val = x; }
     }
 
-    public static ListNode build(BigDecimal num){
-        char[] chars = num.toPlainString().toCharArray();
+    public static ListNode build(String numStr){
+        String[] chars = StringUtils.commaDelimitedListToStringArray(numStr);
         ListNode firstNode = null;
         ListNode preNode = null;
         for(int i=chars.length-1;i>=0;i--){
-            ListNode ln = new ListNode(chars[i]-48);
+            ListNode ln = new ListNode(new Integer(chars[i]));
             if(i==chars.length-1){
                 firstNode = ln;
                 preNode = ln;
@@ -74,13 +125,9 @@ public class TwoNumSumAction {
     }
 
     public static void main(String[] args) {
-        ListNode n1 = build(new BigDecimal("1000000000000000000000000000001"));
-//        do{
-//            System.out.println(n1.val);
-//            n1 = n1.next;
-//        } while(n1!=null);
-        ListNode n2 = build(new BigDecimal("465"));
-        ListNode sumNode = addTwoNumbers(n1,n2);
+        ListNode n1 = build("2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,9");
+        ListNode n2 = build("5,6,4,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,4,3,9,9,9,9");
+        ListNode sumNode = addTwoNumbers2(n1,n2);
         do{
             System.out.println(sumNode.val);
             sumNode = sumNode.next;
